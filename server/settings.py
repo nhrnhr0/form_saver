89a1e8c5b350cf96bb305d8f6de87d7082fea3a0
nhrnhr0 <nhrnhr0@gmail.com>
2021-06-06 12:19:10 +0300

    setting static files for production

--- server/settings.py
+++ server/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 from . import secrects
 
@@ -129,7 +130,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
+STATIC_ROOT= os.path.join(BASE_DIR, "static") # when changed, change allso templates location
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static_cdn"),
+]
+
+MEDIA_URL= '/media/'
+MEDIA_ROOT = os.path.join(STATIC_ROOT, 'media_root/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
